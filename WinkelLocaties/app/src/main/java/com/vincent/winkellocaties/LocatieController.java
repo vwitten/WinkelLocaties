a6259e82475053dc5f839cdf9991e47f41af2172
vincent witten <vincent witten>
2017-02-08 09:50:06 +0100

    Android project aangemaakt. In de view een listview en button geplaats voor de maps.

---
+++ WinkelLocaties/app/src/main/java/com/vincent/winkellocaties/LocatieController.java
@@ -0,0 +1,13 @@
+package com.vincent.winkellocaties;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocatieController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locatie_view);
+    }
+}
